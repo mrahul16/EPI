b15da85af16c8bda2d5c9de538f9c3e66d18e280
mrahul16 <rahul.10.mohan@gmail.com>
2018-10-18 23:34:19 +0530

    Implement 8.1 Merge Two Sorted Lists

--- epi_judge_cpp/sorted_lists_merge.cc
+++ epi_judge_cpp/sorted_lists_merge.cc
@@ -1,15 +1,30 @@
 #include "list_node.h"
 #include "test_framework/generic_test.h"
+
 shared_ptr<ListNode<int>> MergeTwoSortedLists(shared_ptr<ListNode<int>> L1,
                                               shared_ptr<ListNode<int>> L2) {
-  // TODO - you fill in here.
-  return nullptr;
+    shared_ptr<ListNode<int>> head(new ListNode<int>);
+    shared_ptr<ListNode<int>> tempHead = head;
+    while(L1 && L2) {
+        if(L1->data < L2->data) {
+            head->next = L1;
+            L1 = L1->next;
+        }
+        else {
+            head->next = L2;
+            L2 = L2->next;
+        }
+        head = head->next;
+    }
+    shared_ptr<ListNode<int>> remainingList = L1 ? L1 : L2;
+    head->next = remainingList;
+    return tempHead->next;
 }
 
-int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"L1", "L2"};
-  return GenericTestMain(args, "sorted_lists_merge.cc",
-                         "sorted_lists_merge.tsv", &MergeTwoSortedLists,
-                         DefaultComparator{}, param_names);
+int main(int argc, char *argv[]) {
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"L1", "L2"};
+    return GenericTestMain(args, "sorted_lists_merge.cc",
+                           "sorted_lists_merge.tsv", &MergeTwoSortedLists,
+                           DefaultComparator{}, param_names);
 }
