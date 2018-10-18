6391e71ccddeed78ed6eb71fe42d5594a72cf8ce
mrahul16 <rahul.10.mohan@gmail.com>
2018-10-18 13:21:16 +0530

    Implement 6.5 Delete Duplicates from a Sorted Array

--- epi_judge_cpp/sorted_array_remove_dups.cc
+++ epi_judge_cpp/sorted_array_remove_dups.cc
@@ -1,22 +1,38 @@
 #include <vector>
 #include "test_framework/generic_test.h"
 #include "test_framework/timed_executor.h"
+
 using std::vector;
+
 // Returns the number of valid entries after deletion.
-int DeleteDuplicates(vector<int>* A_ptr) {
-  // TODO - you fill in here.
-  return 0;
-}
-vector<int> DeleteDuplicatesWrapper(TimedExecutor& executor, vector<int> A) {
-  int end = executor.Run([&] { return DeleteDuplicates(&A); });
-  A.resize(end);
-  return A;
+int DeleteDuplicates(vector<int> *A_ptr) {
+    // NOTE: Check for empty array, remember to put unique element after insertPos (line 20)
+    vector<int>& A = *A_ptr;
+    if(A.empty()) {
+        return 0;
+    }
+    int e = A[0];
+    int insertPos = 0;
+    int len = A.size();
+    for(int i = 1; i < len; i++) {
+        if(A[i] != e) {
+            e = A[i];
+            A[++insertPos] = e;
+        }
+    }
+    return insertPos + 1;
 }
 
-int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"executor", "A"};
-  return GenericTestMain(
-      args, "sorted_array_remove_dups.cc", "sorted_array_remove_dups.tsv",
-      &DeleteDuplicatesWrapper, DefaultComparator{}, param_names);
+vector<int> DeleteDuplicatesWrapper(TimedExecutor &executor, vector<int> A) {
+    int end = executor.Run([&] { return DeleteDuplicates(&A); });
+    A.resize(end);
+    return A;
+}
+
+int main(int argc, char *argv[]) {
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"executor", "A"};
+    return GenericTestMain(
+            args, "sorted_array_remove_dups.cc", "sorted_array_remove_dups.tsv",
+            &DeleteDuplicatesWrapper, DefaultComparator{}, param_names);
 }
